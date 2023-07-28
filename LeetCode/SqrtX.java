d5377469a0baf59d1d1734f96485c653ec2efaa3
vitor cosso <125705086+vittzx@users.noreply.github.com>
2023-07-28 17:35:36 -0300

    Add files via upload

---
+++ LeetCode/SqrtX.java
@@ -0,0 +1,7 @@
+package broCode.LeetCode;
+// problem: https://leetcode.com/problems/sqrtx/description/
+public class SqrtX {
+    public int mySqrt(int x) {
+        return (int)Math.floor(Math.sqrt(x));
+    }
+}
